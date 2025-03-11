510663b5879ac1e8403021a430574636321d6ee2
Onhofre <onofrebernalsantiago@gmail.com>
2025-03-11 16:15:38 -0500

    subir 2

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ async function main() {
         //valida funcionamiento de la conexion 
         await sequelize.authenticate();
         console.log('la conexion es correcta')
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
-        app.listen(3000);
         console.log("servidor en el puerto", 3000)
     } catch (error) {
         console.error('Error en la conexion a la base de datos.', error);
