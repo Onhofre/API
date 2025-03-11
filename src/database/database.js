d08622950ac23e64d9f9d1e46f5a7e37ba3d81ec
Onhofre <onofrebernalsantiago@gmail.com>
2025-03-11 16:06:58 -0500

    subir

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
+dotenv.config(); // Carga las variables de entorno desde .env
 
-export const sequelize = new Sequelize(
-    //NOMBRE DE LA BASE DE DATOS
-    'projectsdb', 
-    //NOMBRE DE USUARIO
-    'postgres',
-    //CONTRASEÑA
-    '123456789', 
-    {
-    host: 'localhost',
-    dialect: 'postgres'
-}) 
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Necesario para Render
+        },
+    },
+});
